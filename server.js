7fce09e9ea32cfb319c314a300b3a9eef20e0c44
iitung <chewytung@gmail.com>
2025-11-10 17:12:57 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import dotenv from "dotenv";
+import cron from "node-cron";
+
+dotenv.config();
+
+// const { data } = axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=ZJ33VM40ULBSPUH4",)
+//     .then((response) => console.log(response))
+//     .catch((error) => console.log(error))
+
+const BASE = "https://www.alphavantage.co/query"
+const WATCHLIST = ["TSLA", "META", "AAPL", "NVDA", "VOO", "AMZN", "PLTR", "GOOG", "SHOP"]
+
+// ============= Helper to fetch daily prices =====================
+async function getDailyPrices(symbol) {
+    const { data } = await axios.get(BASE, {
+        params: {
+            function: "TIME_SERIES_DAILY",
+            symbol,
+            apikey: process.env.ALPHA_KEY
+        }
+    })
+
+    const series = data["Time Series (Daily)"];
+    if (!series) {
+        console.log(`⚠️ No data for ${symbol}`);
+        return null;
+    }
+
+    const dates = Object.keys(series);
+    const latest = parseFloat(series[dates[0]]["4. close"])         //today
+    const prevDayClose = parseFloat(series[dates[1]["4. close"]])   //yesterday
+    const weekAgoClose = parseFloat(series[dates[5]]["4. close"])   //1week ago
+    const monthAgoClose = parseFloat(series[dates[20]]["4. close"]) //1month ago
+
+    return { latest, prevDayClose, weekAgoClose, monthAgoClose }
+
+
+}
+
+// ============== Send alert to Discord ============
+async function sendDiscordAlert(symbol, message) {
+    const payload = {
+        username: "📉 Stock Alert Bot",
+        embeds: [
+            {
+                title: `${symbol} Alert`,
+                description: message,
+                color: 0xff0000,
+                timestamp: new Date(),
+            },
+        ],
+    };
+
+    await axios.post(process.env.DISCORD_WEBHOOK, payload);
+    console.log(`✅ Sent alert for ${symbol}: ${message}`);
+
+}
+
+async function checkStocks() {
+    for (const symbol in WATCHLIST) {
+        const prices = await getDailyPrices(symbol);
+        if (!prices) continue;
+
+        const { latest, prevDay, weekAgo, monthAgo } = prices;
+
+        const dayChange = ((latest - prevDay) / prevDay) * 100
+        const weekChange = ((latest - weekAgo) / weekAgo) * 100
+        const monthChange = ((latest - monthAgo) / monthAgo) * 100
+
+        if (dayChange <= -2)
+            await sendDiscordAlert(symbol, `${symbol} dropped ${dayChange.toFixed(2)}% today`)
+        if (weekChange <= -10)
+            await sendDiscordAlert(symbol, `${symbol} dropped ${weekChange.toFixed(2)}% today`)
+        if (monthChange <= -10)
+            await sendDiscordAlert(symbol, `${symbol} dropped ${monthChange.toFixed(2)}% today`)
+    }
+}
+
+cron.schedule("00 8 * * 1-5", checkStocks);
